8e3156bdb22bb9ff91ff07bb0661e458ef040812
Nicolas Quezada <nicolasquezada.g@gmail.com>
2024-04-08 10:30:57 -0400

    start & fix error

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -17,20 +17,24 @@ module.exports = {
           type: ApplicationCommandOptionType.String,
         },
       ],
-    callback: async (client, interaction) => {
+    callback: async (client, interaction) => {   
         await interaction.deferReply({ ephemeral : true});
-        const song = interaction.options.get('song').value;
-        ////comprobaciones previas
-        if(!interaction.member.voice?.channel) {
-            return interaction.editReply('❌**Tienes que estar en un canal de voz para ejecutar este comando!**');
+        if (interaction.options.get('song')!= null) {
+            const song = interaction.options.get('song').value;
+            ////comprobaciones previas
+            if(!interaction.member.voice?.channel) {
+                return interaction.editReply('❌**Tienes que estar en un canal de voz para ejecutar este comando!**');
+            }
+            if(song.length<5) {
+                return interaction.editReply(`❌**Tienes que especificar el nombre de una canción!**`);
+            } 
+    
+            const {member, channel} = interaction;
+            client.distube.play(interaction.member.voice?.channel, song, {textChannel: channel, member: member});
+            return interaction.editReply(`🔎**Buscando cancion!**`);
+        } else {
+            return interaction.editReply(`❌**Escribe la wea po oe!**`);
         }
-        if(song.length<5) {
-            return interaction.editReply(`❌**Tienes que especificar el nombre de una canción!**`);
-        } 
-
-        const {member, channel} = interaction;
-        client.distube.play(interaction.member.voice?.channel, song, {textChannel: channel, member: member});
-        return interaction.editReply(`🔎**Buscando cancion!**`);
         
     }
 }
